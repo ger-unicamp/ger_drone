fa4a28fa41a194182323848955853c253b106f5d
EltonCN <eltsu3110@hotmail.com>
2020-10-01 12:45:21 -0300

    Plot simples
    Implementação simples de plot dos dados

--- scripts/mapa.py
+++ scripts/mapa.py
@@ -5,19 +5,35 @@ import rospy
 from ger_drone.msg import Object
 from ger_drone.msg import Identifier
 
+from matplotlib import pyplot as plt
+
+objetoX = []
+objetoY = []
+
+def recebeObjeto(msg):
+
+    objetoX.append(msg.pose.position.x)
+    objetoY.append(msg.pose.position.y)
+
+    pass
+
 if __name__ == '__main__':
     try:
+        rospy.init_node('mapa', anonymous="True")
 
-        #Inscrição para publicar/receber aqui!
+        rospy.Subscriber('objeto_detectado', Object, recebeObjeto)
 
-        rate = rospy.Rate(10) #Define a frequência de execução em Hz
+        rate = rospy.Rate(10) #Define a frequencia de execucao em Hz
 
         while not rospy.is_shutdown():
-            
-            #Código aqui!
+        
+            plt.plot(objetoX, objetoY)
+            plt.show()
 
-            rate.sleep() #Espera o tempo para executar o programa na frequência definida
+            rate.sleep() #Espera o tempo para executar o programa na frequencia definida
 
+        plt.close('all')
 
     except rospy.ROSInternalException:
+        plt.close('all')
         pass
