1cf71e98a0dd40b0a66b6054d7532cee3a7c478e
Pepiovesan100 <pedropiovesan2000@gmail.com>
2020-10-10 21:57:12 -0300

    Update prot.py

--- scripts/prot.py
+++ scripts/prot.py
@@ -7,7 +7,7 @@ from std_srvs.srv import Trigger
 from mrs_msgs.srv import Vec4
 from mrs_msgs.srv import ReferenceStampedSrv
 from geometry_msgs.msg import Point
-from mrs_msgs.srv import String 
+from mrs_msgs.srv import String
 from mrs_msgs.msg import PositionCommand
 from geometry_msgs.msg import Point
 
@@ -21,7 +21,7 @@ def pousar():
    um = rospy.ServiceProxy('/uav1/uav_manager/land', Trigger)
    reqa = Trigger._request_class()
    um(reqa)
-   
+
 
 def decolar():
     print('D')
@@ -35,14 +35,14 @@ def rotina():
     rospy.sleep(12)
     decolar()
     rospy.sleep(12)
-    
+
 
 def voar(a):
     rospy.wait_for_service('/uav1/control_manager/reference')
     tres = rospy.ServiceProxy('/uav1/control_manager/reference',ReferenceStampedSrv)
-    
+
     reqc = ReferenceStampedSrv._request_class()
-    
+
     x =a[0]
     y = a[1]
     z = a[2]
@@ -59,19 +59,19 @@ def velocidade():
     quatro = rospy.ServiceProxy('/uav1/constraint_manager/set_constraints',String)
     reqd = String._request_class()
     reqd.value = 'slow'
-        
+
     quatro(reqd)
 
 def checar(a):
-    global check 
+    global check
     while check == False:
         pt = rospy.wait_for_message('/uav1/control_manager/position_cmd',PositionCommand)
         compara(pt,a)
         print(check)
-    check = False    
+    check = False
 
 def compara(msg,w):
-    global check 
+    global check
     posx = msg.position.x
     posy = msg.position.y
     posz = msg.position.z
@@ -81,16 +81,33 @@ def compara(msg,w):
         check = True
     else:
         check = False
-    return()    
+    return()
 
+def getListObject():
+    rospy.wait_for_service('get_object')
+    cinco = rospy.ServiceProxy('get_object', Identifier)
+    reqc = Identifier._request_class()
 
+    reqc.identifier.state = Identifier.STATE_NOPROCESSADO
+    reqc.identifier.type = Identifier.TYPE_BASE
 
+    response = cinco(reqc)
+    return response.list
+
+def getPose(lista):
+    listaPoses = []
+    for i in lista:
+        a = [i.pose.position.x, i.pose.position.y, i.pose.position.z]
+        listaPoses.append(a)
+
+    return listaPoses
     
+
 if __name__ == '__main__':
     try:
         velocidade()
         pontos = [[2,2,2.5],[2,3.5,2.5],[7.5,3.5,2],[7.5,5.5,2],[2,5,2],[2,7.5,2],[7.5,7.5,2],[8.1,2,2]]
-        
+
         decolar()
 
         rospy.sleep(8)
@@ -98,12 +115,10 @@ if __name__ == '__main__':
         for i in pontos:
             rospy.sleep(2)
             voar(i)
-        
+
         rospy.sleep(4)
-        pousar()    
-        
+        pousar()
+
 
     except rospy.ROSInternalException:
-        pass    
-      
-
+        pass
