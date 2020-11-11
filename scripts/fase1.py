c6ab285d581e2528aa22e1bdf199032831e51245
LucasCMamedes <lucascostamamedes@gmail.com>
2020-11-10 21:07:00 -0300

    Testar para as bases suspensas

--- scripts/fase1.py
+++ scripts/fase1.py
@@ -9,53 +9,40 @@ from mrs_msgs.srv import ReferenceStampedSrv
 from geometry_msgs.msg import Point
 from mrs_msgs.srv import String 
 from mrs_msgs.msg import PositionCommand
-from mavros_msgs.msg import State
-from mavros_msgs.srv import CommandBool
 
 
 from ger_drone.msg import Identifier
 from ger_drone.srv import GetObject
 
-armado = True
+
 
 check = False
 
 rospy.init_node('fase1')
 
 def pousar():
-    while armado == False:
-        pass
-    print('P')
-
-    rospy.wait_for_service('/uav1/uav_manager/land')
-    um = rospy.ServiceProxy('/uav1/uav_manager/land', Trigger)
-    reqa = Trigger._request_class()
-    um(reqa)
+   print('P')
+   rospy.wait_for_service('/uav1/uav_manager/land')
+   um = rospy.ServiceProxy('/uav1/uav_manager/land', Trigger)
+   reqa = Trigger._request_class()
+   um(reqa)
    
 
 def decolar():
-    while armado == False:
-        pass
     print('D')
-
     rospy.wait_for_service('/uav1/uav_manager/takeoff')
     dois = rospy.ServiceProxy('/uav1/uav_manager/takeoff', Trigger)
     reqb = Trigger._request_class()
     dois(reqb)
 
-    
-
 def rotina():
     pousar()
-    rospy.sleep(15)
+    rospy.sleep(10)
     decolar()
     rospy.sleep(10)
     
 
 def voar(a):
-    while armado == False:
-        pass
-
     print(a)
     rospy.wait_for_service('/uav1/control_manager/reference')
     tres = rospy.ServiceProxy('/uav1/control_manager/reference',ReferenceStampedSrv)
@@ -78,7 +65,7 @@ def velocidade():
     rospy.wait_for_service('/uav1/constraint_manager/set_constraints')
     quatro = rospy.ServiceProxy('/uav1/constraint_manager/set_constraints',String)
     reqd = String._request_class()
-    reqd.value = 'fast'
+    reqd.value = 'slow'
         
     quatro(reqd)
 
@@ -118,59 +105,42 @@ def getListObject():
 def getPose(lista):
     listaPoses = []
     for i in lista:
-        a = [i.pose.position.x, i.pose.position.y, i.pose.position.z]
+        #a = [i.pose.position.x, i.pose.position.y, i.pose.position.z]
         a = [i.pose.position.x, i.pose.position.y, 3]
         listaPoses.append(a)
     print(listaPoses)
     for j in listaPoses:
         print(j)
         voar(j)
-        rospy.sleep(4)
+        rospy.sleep(3)
+        preparapouso(j)
         rotina()
 
+def preparapouso(j):
+    a = [j[0],j[1],1]
+    voar(a)
+    rospy.sleep(4)
+    print('desceu')
 
-def armaDrone(msg):
-    global armado
-    if msg.armed == False:
-        armado = False
-        rospy.wait_for_service('/uav1/mavros/cmd/arming')
-        proxy = rospy.ServiceProxy('/uav1/mavros/cmd/arming', CommandBool)
-        req = CommandBool._request_class()
-        
-        req.value = True
 
-        proxy(req)
-
-        decolar()
-
-    else:
-        armado = True
 
 if __name__ == '__main__':
-    rospy.Subscriber('/uav1/mavros/state', State, armaDrone)
     try:
         
         velocidade()
-        #pontos = [[6,2,2.5],[4,2,2.5],[2,2,2.5],[2,3.5,2.5],[4,3.5,2],[6,3.5,2],[7.5,3.5,2],[7.5,5,2],[6,5,2],[4,5,2],[2,5,2],[2,7.5,2],[4,7.5,2],[6,7.5,2],[7.5,7.5,2],[8.1,2,2]]
         #pontos = [[2,0,3],[4,0,3], [6,0,3],[6,-1.5,3] ,[4,-1.5,3] ,[2,-1.5,3],[0,-1.5,3],[0,-3,3], [2,-3,3], [4,-3,3], [6,-3,3],[6,-4.5,3], [4,-4.5,3], [2,-4.5,3],[0,-4.5,3],[0,-6,3],[2,-6,3], [4,-6,3], [6,-6,3]]
         pontos = [[2,0,3],[4,0,3], [6,0,3],[6,-1.2,3] ,[4,-1.2,3] ,[2,-1.2,3],[0,-1.2,3],[0,-2.4,3], [2,-2.4,3], [4,-2.4,3], [6,-2.4,3],[6,-3.6,3], [4,-3.6,3], [2,-3.6,3],[0,-3.6,3],[0,-4.8,3],[2,-4.8,3], [4,-4.8,3], [6,-4.8,3],[6,-6,3],[4,-6,3],[2,-6,3],[0,-6,3]]
-        #decolar()
-
-        #rospy.sleep(8)
-
+        #pontos = [[2,0,3],[4,0,3], [6,0,3],[6,-1.2,3] ,[4,-1.2,3] ,[2,-1.2,3]] para teste
+        
         for i in pontos:
             voar(i)
-            rospy.sleep(1)
+            rospy.sleep(2)
         
         rospy.sleep(2)
-    
-        
-
         getListObject()  
-        
+
         #voltar e finalizar
-        #base = [8.1,-2,2.5]
-        base = [0,0,2]
+        base = [0.1,0,2]
         voar(base)
         rospy.sleep(2)
         pousar()
