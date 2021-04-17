fb4f94905c0a72e3e31eea62bb4b37dc557bbfd4
Pepiovesan100 <pedropiovesan2000@gmail.com>
2021-04-17 19:48:16 -0300

    apagando varios arquivos

--- scripts/guadar.py
+++
@@ -1,9 +0,0 @@
-from ger_drone.msg import Identifier
-from ger_drone.srv import GetObject
-
-
-tres = rospy.ServiceProxy('get_object')
-
-req.identifier.state = Identifier.STATE_NOPROCESSADO
-req.identifier.type = Identifier.TYPE_BASE
-reponse = tres(req)
